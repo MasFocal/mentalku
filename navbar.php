f2acb74c7400e7fdd5bb0720f117f1c46c8d9798
MasFocal <focalfossa2004@gmail.com>
2023-10-25 18:18:59 +0700

    update halaman user

--- navbar.php
+++ navbar.php
@@ -5,9 +5,11 @@
     <nav class="navbar">
         <a href="index.php">Home</a>
         <a href="diagnosa.php">Diagnosa Gejala</a>
-        <a href="informasi-diagnosa.php">Informasi Diagnosa</a>
+        <a href="404.php">Informasi Diagnosa</a>
         <a href="404.php">Contact</a>
+        <!--
         <a href="404.php">Profile</a>
+        -->
         <a href="404.php"><button class="button-login" id="btn-login">Login</button></a>
     </nav>
 </div>
