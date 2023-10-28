73c33bcaa59143422f80e504584a55e7748ee121
MasFocal <focalfossa2004@gmail.com>
2023-10-28 20:14:22 +0700

    Update 28 Oktober 2023

--- navbar.php
+++ navbar.php
@@ -1,15 +1,43 @@
 <div class="header">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <a href="index.php">
         <img src="asset/img/logo.png" id="logo">
     </a>
-    <nav class="navbar">
-        <a href="index.php">Home</a>
-        <a href="diagnosa.php">Diagnosa Gejala</a>
-        <a href="404.php">Informasi Diagnosa</a>
-        <a href="about.php">About</a>
+    <div class="navbar">
+        <a id="menu" href="index.php">Home</a>
+        <a id="menu" href="diagnosa.php">Diagnosa Gejala</a>
+        <a id="menu" href="404.php">Informasi Diagnosa</a>
+        <a id="menu" href="about.php">About</a>
         <!--
         <a href="404.php">Profile</a>
         -->
-        <a href="404.php"><button class="button-login" id="btn-login">Login</button></a>
-    </nav>
+        <a id="menu" href="404.php"><button class="button-login" id="btn-login">Login</button></a>
+        <div class="bars">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+    </div>
+    <div class="dropdown">
+        <li><a id="menu" href="index.php">Home</a></li>
+        <li><a id="menu" href="diagnosa.php">Diagnosa Gejala</a></li>
+        <li><a id="menu" href="404.php">Informasi Diagnosa</a></li>
+        <li><a id="menu" href="about.php">About</a></li>
+        <!--
+        <a href="404.php">Profile</a>
+        -->
+        <li><a id="menu" href="404.php"><button class="button-login" id="btn-login">Login</button></a></li>
+    </div>
+    <script>
+        const toggleBtn = document.querySelector('.bars')
+        const toggleBtnIcon = document.querySelector('.bars i')
+        const dropDownMenu = document.querySelector('.dropdown')
+
+        toggleBtn.onClick = function (){
+            dropDownMenu.classList.toggle('open')
+            const isOpen = dropDownMenu.classList.contains('open')
+
+            toggleBtnIcon.classList = isOpen
+            ? 'fa-solid fa-x'
+            : 'fa-solid fa-bars'
+        }
+    </script>
 </div>
